3be6a0ce2d101a0871b50130a63141ff81bad29b
mikaelbashir <bashirm-y16@mgs.org>
2025-02-21 14:34:48 +0000

    singletons integrated fully

--- app/lib/utils/stripe.ts
+++ app/lib/utils/stripe.ts
@@ -2,19 +2,6 @@
 
 import { Stripe } from "stripe";
 
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
-
-if (!STRIPE_SECRET_KEY) {
-    throw new Error('STRIPE_SECRET_KEY environment variable is not set');
-}
-
-let stripe: Stripe | null = null;
-
-export default async function getStripe(): Promise<Stripe> {
-    if (!stripe) stripe = new Stripe(STRIPE_SECRET_KEY);
-    return stripe;
-}
-
 
 export async function createProduct(subcurrencyAmount: number, productName: string, description: string, stripe: Stripe) {
     try {
