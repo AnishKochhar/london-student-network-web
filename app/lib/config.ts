1d51ec1a72c9a1a31836614072e6a48b9db9da6d
mikaelbashir <bashirm-y16@mgs.org>
2025-09-05 17:33:45 +0100

    correct url

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -36,4 +36,8 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 
 export { redis };
 export { sgMail };
-export const BASE_URL = `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL
+// export const BASE_URL = `https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+export const BASE_URL =
+	process.env.VERCEL_URL
+		? `https://${process.env.VERCEL_URL}`
+		: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
